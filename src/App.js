aa512c05f155bef5c6d8a3c9cd3dc81148dc260f
hughmp <hugh@Hughs-MacBook-Pro-2.local>
2016-12-26 08:45:03 +0000

    playing with styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import {
   Layout,
-  Drawer,
   Header,
   Navigation,
   Content,
@@ -22,6 +21,27 @@ import compilation2 from '../assets/images/carousel-home/compilation2.jpg';
 import compilation3 from '../assets/images/carousel-home/compilation3.jpg';
 import wooden_sign from '../assets/images/carousel-home/wooden_sign.jpg';
 import air from '../assets/images/carousel-home/air.jpg';
+import BRGMenu from 'react-burger-menu';
+
+const BurgerMenu = BRGMenu.bubble;
+
+var styles = {
+  bmMenu: {
+    background: '#373a47',
+    padding: '0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0em'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
 
 /* constants */
 const _renderVideo = () => (
@@ -76,27 +96,9 @@ const App = React.createClass({
             style={{backgroundColor: 'white'}}
             >
             <Menu />
+            <BurgerMenu right styles={styles} />
+
           </Header>
-          <Drawer title="Menu">
-            <Navigation>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-                <a href="">Link</a>
-            </Navigation>
-          </Drawer>
           <Content>
             <ImageGallery
               ref={i => this._imageGallery = i}
