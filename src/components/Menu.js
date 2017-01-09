f50316d1f02df0caf2ae50e6fd2d4db73aeb1222
hughmp <hughmp@users.noreply.github.com>
2017-01-09 21:38:01 +0000

    typewriter bug fix & menu styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,28 +7,65 @@ import MENU_TEXT from '../constants/menu.json';
 import calIcon from '../../assets/images/icons/cal.png';
 import './styles/menu.css';
 
-export default () => (
-  <div className='header-menu-wrapper'>
-    {
-      MENU_TEXT
-      .en
-      .map((menu, index) => (
+export default React.createClass({
+  getInitialState() {
+    return {
+      'about': 'none',
+      'surf-package': 'none',
+      'gallery': 'none',
+      'logistics': 'none',
+      'press': 'none'
+    };
+  },
+  render() {
+    return(
+      <div className='header-menu-wrapper'>
+        {
+          MENU_TEXT
+          .en
+          .map((menu, index) => (
+            <Link
+              id={menu.heading}
+              key={index}
+              className='header-menu-item'
+              activeClassName='menu-item-active'
+              to={`/${menu.heading.toLowerCase().replace(/\s/, '')}`}
+              onMouseOver={() => {
+                const key = menu.heading.toLowerCase().replace(/\s/, '');
+                const nextState = {};
+                nextState[key] = 'flex';
+                this.setState(nextState);
+              }}
+              onMouseLeave={() => {
+                const key = menu.heading.toLowerCase().replace(/\s/, '');
+                const nextState = {};
+                nextState[key] = 'none';
+                this.setState(nextState);
+              }}
+            >
+              <ul
+                className='header-menu-ul'
+                style={{
+                  display: this.state[menu.heading.toLowerCase().replace(/\s/, '')],
+                }}
+              >
+                <li className='header-menu-li'>menu item 1</li>
+                <li className='header-menu-li'>menu item 2</li>
+                <li className='header-menu-li'>menu item 3</li>
+                <li className='header-menu-li'>menu item 4</li>
+              </ul>
+              {menu.heading.toUpperCase()}
+            </Link>
+          ))
+        }
         <Link
-          id={menu.heading}
-          key={index}
-          className='header-menu-item'
-          to={`/${menu.heading.toLowerCase().replace(/\s/, '')}`}
+          className='header-booking-button'
+          to="/booking"
         >
-          {menu.heading.toUpperCase()}
+          <img src={calIcon} role='presentation'></img>
+          RESERVATIONS
         </Link>
-      ))
-    }
-    <Link
-      className='header-booking-button'
-      to="/booking"
-    >
-      <img src={calIcon} role='presentation'></img>
-      CHECK AVAILABILITY
-    </Link>
-  </div>
-);
+      </div>
+    )
+  }
+});
