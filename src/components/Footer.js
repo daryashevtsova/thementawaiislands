cbb010353b9a856b4f1579fec19b05cd7f5b88d1
hughmp <hughmp@users.noreply.github.com>
2017-02-11 19:06:51 +0000

    adding pictograms

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,73 @@
 import React from 'react';
 
-import { Footer, FooterSection, FooterLinkList, FooterDropDownSection } from 'react-mdl';
-
-import twitter from '../../assets/images/social/twitter.svg';
-import facebook from '../../assets/images/social/facebook.svg';
-import instagram from '../../assets/images/social/instagram.svg';
+import './styles/footer.css';
 
 export default () => (
-  <Footer size="mega">
-    <FooterSection type="middle">
-        <FooterDropDownSection title="Features">
-            <FooterLinkList>
-                <a href="#">About</a>
-                <a href="#">Terms</a>
-                <a href="#">Partners</a>
-                <a href="#">Updates</a>
-            </FooterLinkList>
-        </FooterDropDownSection>
-        <FooterDropDownSection title="FAQ">
-            <FooterLinkList>
-                <a href="#">Questions</a>
-                <a href="#">Answers</a>
-                <a href="#">Contact Us</a>
-            </FooterLinkList>
-        </FooterDropDownSection>
-        <FooterDropDownSection title="Social">
-            <FooterLinkList>
-              <img src={twitter} role='presentation' />
-              <img src={facebook} role='presentation' />
-              <img src={instagram} role='presentation' />
-            </FooterLinkList>
-        </FooterDropDownSection>
-    </FooterSection>
-    <FooterSection type="bottom" logo="Matungou">
-      <FooterLinkList>
-          <a href="#">Help</a>
-          <a href="#">Privacy & Terms</a>
-      </FooterLinkList>
-    </FooterSection>
-  </Footer>
+    <footer className='content md dark-1 flex-column'>
+        <div class="test-brd">
+            <div class="">
+                {/*<div class="contact">
+                    <h2>Contact</h2>
+                    <ul class="contact-icons">
+                        <li class="icon fa-home">
+                            <a href="#">
+                                234 Fictional Street 5432<br />Nashville, TN 00000-0000
+                            </a>
+                        </li>
+                        <li class="icon fa-phone">
+                            <a href="#">(000) 000-0000</a>
+                        </li>
+                        <li class="icon fa-envelope-o">
+                            <a href="#">info@untitled.tld</a>
+                        </li>
+                    </ul>
+                    <ul class="icons-bordered">
+                        <li>
+                            <a class="icon fa-facebook" href="#">
+                                <span class="label">Facebook</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="icon fa-twitter" href="#">
+                                <span class="label">Twitter</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="icon fa-instagram" href="#">
+                                <span class="label">Instagram</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="icon fa-github" href="#">
+                                <span class="label">GitHub</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="icon fa-linkedin" href="#">
+                                <span class="label">LinkedIn</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>*/}
+                {/*<form action="#" method="post">
+                    <h2>Email Us</h2>
+                    <div class="field half first">
+                        <input name="name" id="name" placeholder="Name" type="text" />
+                    </div>
+                    <div class="field half">
+                        <input name="email" id="email" placeholder="Email" type="email" />
+                    </div>
+                    <div class="field">
+                        <textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+                    </div>
+                    <ul class="actions">
+                        <li><input value="Send Message" class="button" type="submit" /></li>
+                    </ul>
+                </form>*/}
+            </div>
+            <div class="copyright">
+                <p>&copy; Untitled. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
 );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,38 @@
 import React from 'react';
 
 import Banner from './Banner';
+import Footer from './Footer';
 
-import wave from '../../assets/images/home/wave_collage.jpg';
+// images
+import wave_collage from '../../assets/images/home/wave_collage.jpg';
+import trip from '../../assets/images/home/trip.jpg';
+import group from '../../assets/images/pictograms/group.png';
+import destination from '../../assets/images/pictograms/destination.png';
+import airport from '../../assets/images/pictograms/airport.png';
+import accommodation from '../../assets/images/pictograms/accommodation.png';
+import meals from '../../assets/images/pictograms/meals.png';
+import surf from '../../assets/images/pictograms/surf.png';
+import wave from '../../assets/images/pictograms/wave.png';
+import ability from '../../assets/images/pictograms/ability.png';
+import suited from '../../assets/images/pictograms/suited.png';
+import line from '../../assets/images/pictograms/line.png';
 
 import './styles/home.css';
 
 export default ({setTransparent}) => (
   <div className='home-wrapper'>
+
     <Banner
       setTransparent={setTransparent}
     />
+
     <div className='content'>
+
       <div
         className='wave'
-        style={{ backgroundImage: `url(${wave})` }}
+        style={{ backgroundImage: `url(${wave_collage})` }}
       />
+
       <div className='wave-text'>
         <h1>
           AFFORDABLE ACCOMMODATION,
@@ -27,15 +44,102 @@ export default ({setTransparent}) => (
           - MATUNGOU, THE MENTAWAI ISLANDS
         </h2>
       </div>
+
     </div>
-    <div className='content dark'>
-      CONTENT 2
+
+    <div className='content flex-row md-l'>
+      <div
+        className='trip-img'
+        style={{ backgroundImage: `url(${trip})` }}
+      />
+      <div className='trip-content'>
+
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${group})` }}
+          />
+          <p><b>Camp Size </b><br /><br />Max 8 Guests</p>
+        </div>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${destination})` }}
+          />
+          <p><b>Destination </b><br /><br />Northern Mentawai Islands, Indonesia</p>
+        </div>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${airport})` }}
+          />
+          <p><b>Airport </b><br /><br />Padang Minangkabau International , Western Sumatra</p>
+        </div>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${accommodation})` }}
+          />
+          <p><b>Accommodation </b><br /><br />Private Beachside Bungalows - 2/3 person</p>
+        </div>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${meals})` }}
+          />
+          <p><b>Meals: </b><br /><br />Breakfast, lunch and Dinner Included, Indonesian Style</p>
+        </div>
+      </div>
+
+      <div className='trip-content'>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${surf})` }}
+          />
+          <p><b>Surf Access </b><br /><br />Private daily surf boat and Guide (just you and your crew)</p>
+        </div>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${wave})` }}
+          />
+          <p><b>Waves </b><br /><br />Playgrounds, Northern Mentawai - 20+ waves within 30 minutes<br /><span className='kandui'>No Kandui</span>, Rifles, Bankvaults, Ebay are a few local big names</p>
+        </div>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${ability})` }}
+          />
+          <p><b>Surf Ability </b><br /><br />Intermediate to Professional</p>
+        </div>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${suited})` }}
+          />
+          <p><b>Best Suited For </b><br /><br />Medium-Budget travellers, Couples, Groups & Families</p>
+        </div>
+        <div className='trip-desc'>
+          <div
+            className='trip-pic'
+            style={{ backgroundImage: `url(${line})` }}
+          />
+          <p><b>Bottom-line </b><br /><br />Unbeatable value if you wish to experience a premium surf location without the commonly nauseating price-tag</p>
+        </div>
+      </div>
+
     </div>
+
     <div className='content'>
       CONTENT 3
     </div>
-    <div className='content dark'>
+
+    <div className='content md dark-2'>
       CONTENT 4
     </div>
+
+    <Footer />
+
   </div>
 );
