0112cf95a25f7805f50af5393347348abdfde871
hughmp <hughmp@users.noreply.github.com>
2017-02-15 21:35:07 +0000

    auto generated aspect ratios for gallery

--- src/constants/images.js
+++ src/constants/images.js
@@ -94,342 +94,344 @@ import i47_medium from '../../assets/images/gallery/highlights/medium/i47.jpg';
 import i47_large from '../../assets/images/gallery/highlights/large/i47.jpg';
 import i48_medium from '../../assets/images/gallery/highlights/medium/i48.jpg';
 import i48_large from '../../assets/images/gallery/highlights/large/i48.jpg';
+import i49_medium from '../../assets/images/gallery/highlights/medium/i49.jpg';
+import i49_large from '../../assets/images/gallery/highlights/large/i49.jpg';
 
 const IMAGES = [
   {
-    src: i1_large,
-    thumbnail: i1_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i2_large,
-    thumbnail: i2_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i3_large,
-    thumbnail: i3_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i4_large,
-    thumbnail: i4_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i5_large,
-    thumbnail: i5_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i6_large,
-    thumbnail: i6_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i7_large,
-    thumbnail: i7_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i8_large,
-    thumbnail: i8_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i9_large,
-    thumbnail: i9_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i10_large,
-    thumbnail: i10_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i11_large,
-    thumbnail: i11_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i12_large,
-    thumbnail: i12_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i13_large,
-    thumbnail: i13_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i14_large,
-    thumbnail: i14_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i15_large,
-    thumbnail: i15_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i16_large,
-    thumbnail: i16_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i17_large,
-    thumbnail: i17_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i18_large,
-    thumbnail: i18_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i19_large,
-    thumbnail: i19_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i20_large,
-    thumbnail: i20_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i21_large,
-    thumbnail: i21_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i22_large,
-    thumbnail: i22_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i23_large,
-    thumbnail: i23_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i24_large,
-    thumbnail: i24_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i25_large,
-    thumbnail: i25_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i26_large,
-    thumbnail: i26_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i27_large,
-    thumbnail: i27_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i28_large,
-    thumbnail: i28_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i29_large,
-    thumbnail: i29_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i30_large,
-    thumbnail: i30_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i31_large,
-    thumbnail: i31_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i32_large,
-    thumbnail: i32_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i33_large,
-    thumbnail: i33_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i34_large,
-    thumbnail: i34_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i35_large,
-    thumbnail: i35_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i36_large,
-    thumbnail: i36_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i37_large,
-    thumbnail: i37_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i38_large,
-    thumbnail: i38_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i39_large,
-    thumbnail: i39_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i40_large,
-    thumbnail: i40_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i41_large,
-    thumbnail: i41_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i42_large,
-    thumbnail: i42_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i43_large,
-    thumbnail: i43_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i44_large,
-    thumbnail: i44_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    caption: "INSERT_CAPTION"
-  },
-  {
-    src: i45_large,
-    thumbnail: i45_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
+    src: i47_large,
+    thumbnail: i47_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 426,
     caption: "INSERT_CAPTION"
   },
   {
     src: i46_large,
     thumbnail: i46_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
     caption: "INSERT_CAPTION"
   },
   {
-    src: i47_large,
-    thumbnail: i47_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
+    src: i45_large,
+    thumbnail: i45_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
     caption: "INSERT_CAPTION"
   },
   {
-    src: i48_large,
-    thumbnail: i48_medium,
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
+    src: i44_large,
+    thumbnail: i44_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i43_large,
+    thumbnail: i43_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i42_large,
+    thumbnail: i42_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i41_large,
+    thumbnail: i41_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i40_large,
+    thumbnail: i40_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i39_large,
+    thumbnail: i39_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i38_large,
+    thumbnail: i38_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i37_large,
+    thumbnail: i37_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i36_large,
+    thumbnail: i36_medium,
+    thumbnailWidth: 427,
+    thumbnailHeight: 640,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i35_large,
+    thumbnail: i35_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i34_large,
+    thumbnail: i34_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i33_large,
+    thumbnail: i33_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i32_large,
+    thumbnail: i32_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i31_large,
+    thumbnail: i31_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i30_large,
+    thumbnail: i30_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i29_large,
+    thumbnail: i29_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i28_large,
+    thumbnail: i28_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 448,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i27_large,
+    thumbnail: i27_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i26_large,
+    thumbnail: i26_medium,
+    thumbnailWidth: 427,
+    thumbnailHeight: 640,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i25_large,
+    thumbnail: i25_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i24_large,
+    thumbnail: i24_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i23_large,
+    thumbnail: i23_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i22_large,
+    thumbnail: i22_medium,
+    thumbnailWidth: 427,
+    thumbnailHeight: 640,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i21_large,
+    thumbnail: i21_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i20_large,
+    thumbnail: i20_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 424,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i19_large,
+    thumbnail: i19_medium,
+    thumbnailWidth: 503,
+    thumbnailHeight: 640,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i18_large,
+    thumbnail: i18_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i17_large,
+    thumbnail: i17_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i16_large,
+    thumbnail: i16_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i15_large,
+    thumbnail: i15_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i14_large,
+    thumbnail: i14_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i13_large,
+    thumbnail: i13_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i12_large,
+    thumbnail: i12_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i11_large,
+    thumbnail: i11_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i10_large,
+    thumbnail: i10_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i9_large,
+    thumbnail: i9_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 439,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i8_large,
+    thumbnail: i8_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i7_large,
+    thumbnail: i7_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i6_large,
+    thumbnail: i6_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i5_large,
+    thumbnail: i5_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 427,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i4_large,
+    thumbnail: i4_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 428,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i3_large,
+    thumbnail: i3_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 360,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i2_large,
+    thumbnail: i2_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 360,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i1_large,
+    thumbnail: i1_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 360,
+    caption: "INSERT_CAPTION"
+  },
+  {
+    src: i49_large,
+    thumbnail: i49_medium,
+    thumbnailWidth: 640,
+    thumbnailHeight: 426,
     caption: "INSERT_CAPTION"
   }
 ];
